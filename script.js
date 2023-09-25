58df46b7c4268297f4cbdc69c6437acd47587f1b
Gustavo Verdi <140762138+GustavoVerdi@users.noreply.github.com>
2023-09-25 17:30:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function mostrarSubtela() {
+    var subtela = document.getElementById("minhaSubtela");
+    subtela.style.display = "block";
+}
+
+// Função para fechar a subtela de checkout
+function fecharSubtela() {
+    var subtela = document.getElementById("minhaSubtela");
+    subtela.style.display = "none";
+}
